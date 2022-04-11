5e1f7f89d3e32ded7bd85411fe68465a187c6fcf
Alex Drozd <drozdster@gmail.com>
2022-04-11 09:23:31 -0500

    showing example

---
+++ app.py
@@ -0,0 +1,41 @@
+import responses
+import requests
+
+from decorator import decorator
+
+GOOGLE_URL = "https://www.google.com/"
+YOUTUBE_URL = "https://www.youtube.com/"
+
+
+@decorator
+def mock_google(func, *args, **kwargs):
+    with responses.RequestsMock(assert_all_requests_are_fired=False) as rsps:
+        rsps.add(responses.GET, GOOGLE_URL, json={"success": True})
+        return func(*args, **kwargs)
+
+
+def business_logic():
+    response = requests.get(GOOGLE_URL)
+    payload = response.json()
+    assert payload["success"]
+    response = requests.get(YOUTUBE_URL)
+    payload = response.json()
+    assert payload["success"]
+
+
+@mock_google
+@responses.activate
+def test_stuff():
+    responses.add(
+        responses.Response(method="POST", url=YOUTUBE_URL, json={"success": True})
+    )
+    business_logic()
+
+
+@responses.activate
+@mock_google
+def test_stuff_in_a_different_order():
+    responses.add(
+        responses.Response(method="POST", url=YOUTUBE_URL, json={"success": True})
+    )
+    business_logic()
